7af9e6167706bacc9f55deb4eeec60544c119e24
Nan <xiaonan@yahoo.com>
2020-11-30 15:34:45 -0800

    Add home

--- src/main/java/demoapimaven/demo/controller/DemoController.java
+++ src/main/java/demoapimaven/demo/controller/DemoController.java
@@ -5,6 +5,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
+	@RequestMapping("/") 
+	public String home() {
+		return "Hello to Demo App";
+	}
+	
     @RequestMapping("/getInfo")
     public String getInfo() {
         return "Demo API Version 1.0";
